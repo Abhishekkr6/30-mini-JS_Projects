1d6a9b6ff2896bad38eaf3177d6a1907e753ea44
Abhishek Tiwari <tiwariabhi62065@gmail.com>
2024-08-17 14:36:06 +0530

    some changes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-let follower = document.querySelector(".follower")
+let follower = document.querySelector(".follower");
 
 document.addEventListener('mousemove', function(e) {
     gsap.to('.follower', {
@@ -10,579 +9,107 @@ document.addEventListener('mousemove', function(e) {
     });
 });
 
-let heading = document.querySelector(".head")
-let p1 = document.querySelector("#box1 img")
-let p2 = document.querySelector("#box2 img")
-let p3 = document.querySelector("#box3 img")
-let p4 = document.querySelector("#box4 img")
-let p5 = document.querySelector("#box5 img")
-let p6 = document.querySelector("#box6 img")
-let p7 = document.querySelector("#box7 img")
-let p8 = document.querySelector("#box8 img")
-let p9 = document.querySelector("#box9 img")
-let p10 = document.querySelector("#box10 img")
-let p11 = document.querySelector("#box11 img")
-let p12 = document.querySelector("#box12 img")
-let p13 = document.querySelector("#box13 img")
-let p14 = document.querySelector("#box14 img")
-let p15 = document.querySelector("#box15 img")
-let p16 = document.querySelector("#box16 img")
-let p17 = document.querySelector("#box17")
-let p18 = document.querySelector("#box18")
-let p19 = document.querySelector("#box19")
-let p20 = document.querySelector("#box20")
-let p21 = document.querySelector("#box21")
-let p22 = document.querySelector("#box22")
-let p23 = document.querySelector("#box23")
-let p24 = document.querySelector("#box24")
-let p25 = document.querySelector("#box25")
-let p26 = document.querySelector("#box26")
-let p27 = document.querySelector("#box27")
-let p28 = document.querySelector("#box28")
-let p29 = document.querySelector("#box29")
-let p30 = document.querySelector("#box30")
+let heading = document.querySelector(".head");
+let projectImages = [];
+for (let i = 1; i <= 16; i++) {
+    projectImages.push(document.querySelector(`#box${i} img`));
+}
 
-
-
+let comingSoon = [];
+for (let i = 17; i <= 30; i++) {
+    comingSoon.push(document.querySelector(`#box${i}`));
+}
 
 heading.addEventListener("mouseenter", () => {
     gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
+        duration: .5, 
+        scale: 2.2,
+        backgroundColor: "rgba(255, 255, 255, 0.5)"
+    });
+});
+
 heading.addEventListener("mouseleave", () => {
     gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p1.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_1"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p1.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p2.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_2"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p2.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p3.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_3"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p3.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p4.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_4"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p4.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p5.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_5"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p5.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p6.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_6"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p6.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p7.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_7"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p7.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p8.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_8"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p8.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p9.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_9"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p9.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p10.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_10"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p10.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p11.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_11"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p11.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p12.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_12"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p12.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p13.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_13"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p13.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p14.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_14"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p14.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p15.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_15"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p15.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p16.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_16"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p16.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p17.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_17"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p17.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p18.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_18"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p18.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p19.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_19"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p19.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p20.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Project_20"
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p20.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p21.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p21.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p22.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p22.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p23.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p23.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p24.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p24.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p25.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p25.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p26.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p26.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p27.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p27.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p28.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p28.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p29.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p29.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
-p30.addEventListener("mouseenter", () => {
-    follower.innerHTML = "Coming..."
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 2.2,
-          backgroundColor: "rgba(255, 255, 255, 0.5)"
-        });
-})
-p30.addEventListener("mouseleave", () => {
-    follower.innerHTML = ""
-    gsap.to('.follower', {
-          duration: .5, 
-          scale: 1,
-          backgroundColor: "rgba(53, 53, 53, 0.5)"
-        });
-})
+        duration: .5, 
+        scale: 1,
+        backgroundColor: "rgba(53, 53, 53, 0.5)"
+    });
+});
 
+projectImages.forEach((img, index) => {
+    img.addEventListener("mouseenter", () => {
+        follower.innerHTML = `Project_${index + 1}`;
+        gsap.to('.follower', {
+            duration: .5, 
+            scale: 2.2,
+            backgroundColor: "rgba(255, 255, 255, 0.5)"
+        });
+    });
 
+    img.addEventListener("mouseleave", () => {
+        follower.innerHTML = "";
+        gsap.to('.follower', {
+            duration: .5, 
+            scale: 1,
+            backgroundColor: "rgba(53, 53, 53, 0.5)"
+        });
+    });
+});
 
+comingSoon.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+        follower.innerHTML = "Coming...";
+        gsap.to('.follower', {
+            duration: .5, 
+            scale: 2.2,
+            backgroundColor: "rgba(255, 255, 255, 0.5)"
+        });
+    });
 
+    box.addEventListener("mouseleave", () => {
+        follower.innerHTML = "";
+        gsap.to('.follower', {
+            duration: .5, 
+            scale: 1,
+            backgroundColor: "rgba(53, 53, 53, 0.5)"
+        });
+    });
+});
 
+// Animation
+let tl = gsap.timeline();
 
-
-                      // Animation
-let tl = gsap.timeline()
-
-tl.from(".navbar",{
+tl.from(".navbar", {
     y: -50,
     scale: -1,
     delay: .2,
     duration: 1.5 
 })
-tl.from(".navbar h1",{
+.from(".navbar h1", {
     y: -50,
     duration: 1,
     opacity: 0
-})
+});
 
-gsap.from(".box",{
-    // x: 65,
+gsap.from(".box", {
     scale: 0,
     opacity: 0,
     duration: 1,
     delay: 1,
     stagger: .4,
-})
+});
 
-
-gsap.from("footer",{
+gsap.from("footer", {
     y: 100,
     opacity: 0,
     duration: 1.5,
     stagger: .8,
     delay: .6,
-    
-})
-gsap.from(".social i, footer h3",{
-    // x: 65,
+});
+
+gsap.from(".social i, footer h3", {
     scale: 0,
     opacity: 0,
     duration: 1.2,
     delay: 2
-})
+});
